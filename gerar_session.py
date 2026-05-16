43191e4c50662137cfdbd9ac0e4ec20fd2c5bd2b
davidabutre-bit <david.abutre@gmail.com>
2026-05-16 11:59:36 -0400

    Update gerar_session.py

--- gerar_session.py
+++ gerar_session.py
@@ -3,7 +3,7 @@ from telethon.sync import TelegramClient
 api_id = 36525640
 api_hash = "25bfdf0065ba1025cd97c226076d69b6"
 
-SESSION_NAME = "coutips_v2_session"
+SESSION_NAME = "coutips_v2_session.session"
 
 client = TelegramClient(
     SESSION_NAME,
